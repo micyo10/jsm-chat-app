ee92a8ae4a06839b81b969ecf124c49d0acda484
Mike Yo <mikeyo@bigvoodoo.com>
2019-12-16 23:10:46 -0500

    1:37:18 / 1:56:44

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
+//require functions from users.js file
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users.js');
+
 const PORT = process.env.PORT || 5000;
 
 const router = require('./router');
@@ -10,21 +13,39 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+// on function takes two parameters, a string, and an arrow function
+
 io.on('connection', (socket) => {
-  console.log('a user connected');
-
+  // console.log('a user connected');
   socket.on('join', ({ name, room }, callback) => {
-    console.log(name, room);
+    console.log('New user, ' + name + ', joined ' + room);
 
-    // error testing
-    // const error = true;
+    const { error, user } = addUser({ id: socket.id, name, room });
+    // if error, exit function
+    if(error) return callback(error);
+    // otherwise, continue
 
-    // if(error) {
-    //   callback({ error: 'error'});
-    // }
+    // sends welcome message to user
+    socket.emit('message', {user: 'admin', text: `${user.name}, welcome to the room ${user.room}`});
+    // broadcast method sends a message to everyone in the room except specific user
+    socket.broadcast.to(user.room).emit('message', {user:'admin', text: `${user.name} has joined!`});
+
+    // join function joins a user to a room, takes room from the user array, room item
+    socket.join(user.room);
 
     callback();
   })
+
+  // expecting event "on" something happening
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit('message', {user: user.name, text: message});
+
+    callback();
+  });
+
+
 
   socket.on('disconnect', () => {
     console.log('a user disconnected');
